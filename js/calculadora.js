56c2dbe43d5c2004ddbd2753d9399dc76d1bfbe6
Hectorcg88 <casesgimenezh@gmail.com>
2023-05-09 19:50:31 +0200

    Funcionalidad añadida js

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,6 +4,10 @@ const numeros = document.querySelectorAll("#numero");
 const operaciones = document.querySelectorAll("#operacion");
 const borrar = document.getElementById("borra");
 const resultado = document.getElementById("igual");
+const coseno = document.getElementById("coseno");
+const seno = document.getElementById("seno");
+const tan = document.getElementById("tan");
+const raiz = document.getElementById("raiz")
 
 // Esto sirve para recorrer el array completo y al hacer click el valor se agrega al screen (es para los numeros)
 for (let i = 0; i < numeros.length; i++) {
@@ -29,6 +33,39 @@ resultado.addEventListener("click", function() {
   const escrito = screen.value;
   if (escrito) {
     const resultado = eval(escrito);
+    screen.value = resultado;
+  }
+});
+
+// Con este evento se puede calcular el coseno del valor que hay en pantalla
+coseno.addEventListener("click", function() {
+  const escrito = screen.value;
+  if (escrito) {
+    const resultado = Math.cos(eval(escrito));
+    screen.value = resultado;
+  }
+});
+
+seno.addEventListener("click", function() {
+  const escrito = screen.value;
+  if (escrito) {
+    const resultado = Math.sin(eval(escrito));
+    screen.value = resultado;
+  }
+});
+
+tan.addEventListener("click", function() {
+  const escrito = screen.value;
+  if (escrito) {
+    const resultado = Math.tan(eval(escrito));
+    screen.value = resultado;
+  }
+});
+
+raiz.addEventListener("click", function() {
+  const escrito = screen.value;
+  if (escrito) {
+    const resultado = Math.sqrt(eval(escrito));
     screen.value = resultado;
   }
 });
